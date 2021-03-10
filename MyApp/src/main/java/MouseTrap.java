07cdf2c40b486146cf3c1c08c6d91f7332b1c7bf
Scott Luu <sla344@sfu.ca>
2021-03-09 19:23:30 -0800

    Mousetrap

--- MyApp/src/main/java/MouseTrap.java
+++ MyApp/src/main/java/MouseTrap.java
@@ -1,2 +1,32 @@
-public class MouseTrap {
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MouseTrap extends Enemies {
+    private int penalty;
+    private BufferedImage MTImage;
+    
+    MouseTrap(){
+        this.penalty = 6; 
+        this.currentX = getCurrentX();
+        this.currentY = getCurrentY();
+
+        try {
+            Path MTPath = Paths.get("MyApp/Images/mousetrap.jpg").toRealPath();;
+            this.MTImage = ImageIO.read(new File(MTPath.toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getPenalty(){
+        return penalty;
+    }
+
+    public BufferedImage getMTImage () {
+        return MTImage;
+    }
 }
