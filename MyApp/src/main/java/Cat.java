40e897ab6011f4a1238c83cf36b7852431ea5408
Scott Luu <sla344@sfu.ca>
2021-03-13 19:40:50 -0800

    Cat

--- MyApp/src/main/java/Cat.java
+++ MyApp/src/main/java/Cat.java
@@ -1,69 +1,50 @@
-package main.java;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Cat {
     private int currentX;
     private int currentY;
     private final int catHeight = 25;
     private final int catWidth = 25;
-    private BufferedImage catImage;
-
 
     Cat(int startX, int startY)
     {
         this.currentX = startX;
         this.currentY = startY;
-        try {
-            Path catPath = Paths.get("MyApp/Images/cat.png").toRealPath();;
-            this.catImage = ImageIO.read(new File(catPath.toString()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public int chase (int mouseCurrentX, int mouseCurrentY, Tile [][] map) {
         int distanceX = mouseCurrentX - this.getCurrentX();
         int distanceY = mouseCurrentY - this.getCurrentY();
         //if Mouse is on the right
-        if (distanceX > 0) {
+        while (distanceX > 0) {
             if (map[this.getCurrentY()][this.getCurrentX() + 1].getisBarrier() == false) {
                     this.setCurrentX(this.getCurrentX() + 1);
                     return 1;
             }
-            else{
-
-                    if(map[this.getCurrentY()-1][this.getCurrentX()].getisBarrier() == false){
+            else if(!map[this.getCurrentY()-1][this.getCurrentX()].getisBarrier()){
                         this.setCurrentY(this.getCurrentY() - 1);
                             System.out.println("return 4");
                             return 4;
                     }
-                    else if (map[this.getCurrentY()][this.getCurrentX() - 1].getisBarrier() == false) {
+            else if (map[this.getCurrentY()][this.getCurrentX() - 1].getisBarrier() == false) {
                         this.setCurrentX(this.getCurrentX() - 1);
                         return 2;
-                    }
-                    else {
+            }
+            else if(!map[this.getCurrentY()+1][this.getCurrentX()].getisBarrier()){
                         this.setCurrentY(this.getCurrentY() + 1);
                         System.out.println("return 3");
                         return 3;
-                    }
-
+            }
+            else{
+                return 0;
             }
 
-
-        } else if (distanceX < 0){
+        
+        } 
+        while (distanceX < 0){
             if (map[this.getCurrentY()][this.getCurrentX() - 1].getisBarrier() == false) {
                 this.setCurrentX(this.getCurrentX() - 1);
                 return 2;
             }
-            else{
-
-                if(map[this.getCurrentY()-1][this.getCurrentX()].getisBarrier() == false){
+            else if(map[this.getCurrentY()-1][this.getCurrentX()].getisBarrier() == false){
                     this.setCurrentY(this.getCurrentY() - 1);
                         System.out.println("return 4");
                         return 4;
@@ -72,72 +53,74 @@ public class Cat {
                     this.setCurrentX(this.getCurrentX() + 1);
                     return 1;
                 }
-                else {
+                else if(map[this.getCurrentY()+1][this.getCurrentX()].getisBarrier() == false){
                     this.setCurrentY(this.getCurrentY() + 1);
                     System.out.println("return 3");
                     return 3;
                 }
+                else{
+                    return 0;
+                }
 
-            }
+            
 
-
-
-        } else {
-            if (distanceY > 0 ) {
+        } 
+        while (distanceY > 0 && distanceX == 0) {
                 if (map[this.getCurrentY() + 1][this.getCurrentX()].getisBarrier() == false) {
                     this.setCurrentY(this.getCurrentY() + 1);
                     System.out.println("return 3");
                     return 3;
                 }
 
-                else{
-
-                    if(map[this.getCurrentY()-1][this.getCurrentX()].getisBarrier() == false){
-                        this.setCurrentY(this.getCurrentY() - 1);
-                            System.out.println("return 4");
-                            return 4;
+                else if(map[this.getCurrentY()][this.getCurrentX()-1].getisBarrier() == false){
+                        this.setCurrentX(this.getCurrentX() - 1);
+                            System.out.println("return 2");
+                            return 2;
                     }
                     else if (map[this.getCurrentY()][this.getCurrentX() + 1].getisBarrier() == false) {
                         this.setCurrentX(this.getCurrentX() + 1);
                         return 1;
                     }
-                    else {
+                    else if (map[this.getCurrentY() - 1][this.getCurrentX()].getisBarrier() == false) {
                         this.setCurrentY(this.getCurrentY() - 1);
-                        System.out.println("return 2");
-                        return 2;
+                        System.out.println("return 4");
+                        return 4;
+                    }
+                    else{
+                        return 0;
                     }
     
-                }
+                
 
 
-            } else if (distanceY < 0 ){
+        } while (distanceY < 0 && distanceX == 0 ){
                 if (map[this.getCurrentY() - 1][this.getCurrentX()].getisBarrier() == false) {
                     this.setCurrentY(this.getCurrentY() - 1);
                     System.out.println("return 4");
                     return 4;
                 }
-                else{
-
-                    if(map[this.getCurrentY()][this.getCurrentX() + 1].getisBarrier() == false){
+                else if(map[this.getCurrentY()+1][this.getCurrentX() ].getisBarrier() == false){
                         this.setCurrentY(this.getCurrentY() + 1);
                             System.out.println("return 3");
                             return 3;
-                    }
+                }
                     else if (map[this.getCurrentY()][this.getCurrentX() + 1].getisBarrier() == false) {
                         this.setCurrentX(this.getCurrentX() + 1);
                         return 1;
                     }
-                    else {
-                        this.setCurrentY(this.getCurrentY() - 1);
+                    else if (map[this.getCurrentY()][this.getCurrentX() - 1].getisBarrier() == false) {
+                        this.setCurrentX(this.getCurrentX() - 1);
                         System.out.println("return 2");
                         return 2;
                     }
+                    else{
+                        return 0;
+                    }
     
-                }
-            } else {
+                
+            } 
                 return 0;
-            }
-        }
+
     }
 
     public int getCurrentX() {
@@ -154,8 +137,5 @@ public class Cat {
     public void setCurrentY(int currentY) {
         this.currentY = currentY;
     }
-
-    public BufferedImage getCatImage () {
-        return catImage;
-    }
 }
+
