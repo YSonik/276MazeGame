29c5b72cd188fcec6e7f93bc1258b16697dc80a1
Canh Le <lecanhnhatminh@gmail.com>
2021-03-20 15:35:00 +0700

    Fixing issues

--- MyApp/src/main/java/main/java/Game.java
+++ MyApp/src/main/java/main/java/Game.java
@@ -127,6 +127,7 @@ public class Game extends JFrame implements KeyListener {
         f.setVisible(true);
     }
 
+
     public void losePage() {
         this.dispose();
         JFrame f = new JFrame("Lose");
