b5efb523ef4ab7b02b45d2ff8cb8207422edf4a0
Scott Luu <sla344@sfu.ca>
2021-04-06 19:46:15 -0700

    Entrance + Exit + Structure 1 & 2 check

--- MyApp/src/test/java/TestGenMap.java
+++ MyApp/src/test/java/TestGenMap.java
@@ -5,48 +5,103 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestGenMap {
     
     private LevelOne map;
+    Tile[][] layout;
+    Boolean check;
+
+
     @BeforeEach
     void setup(){
         map = new LevelOne();
+        layout = map.getLevelMap();
+        check = true;
     }
 
     @Test
     public void checkMapBoundary() {
         //map = new LevelOne();
-        Tile[][] getb = map.getLevelMap();
-        Boolean check = true;
+        
         
         //check Left column
         for (int i = 2; i< 10; i++){
-            if(!(getb[i][0].getisBarrier())){
+            if(!(layout[i][0].getisBarrier())){
                 check  = false;
+                break;
             }
         }
         //check Top row
         for(int i = 0; i < 10; i++)
         {
-            if(!(getb[0][i].getisBarrier())){
+            if(!(layout[0][i].getisBarrier())){
                 check  = false;
+                break;
             }
         }
-
         //check Bottom Row
         for(int i = 0; i<10;i++)
         {
-            if(!(getb[9][i].getisBarrier())){
+            if(!(layout[9][i].getisBarrier())){
                 check  = false;
+                break;
             }
         }
 
         //check Right Column
-        for (int i = 0; i< 7; i++){
-            if(!(getb[i][9].getisBarrier())){
+        for (int i = 0; i< 8; i++){
+            if(!(layout[i][9].getisBarrier())){
                 check  = false;
+                break;
             }
         }
 
 
         assertEquals(true, check);
     }
+
+    @Test
+    public void checkExit(){
+        if(!(layout[1][0].getisExit())){
+            check = false;
+        }
+        assertEquals(true, check);
+    }
+
+    @Test
+    public void checkEntrance(){
+        if(!(layout[8][9].getisEntrance())){
+            check = false;
+        }
+        assertEquals(true, check);
+    }
+
+    @Test
+    public void checkStructure(){
+        
+        //check Structure1
+        for(int i = 2; i <5;i++ )
+        {   if(!(layout[2][i].getisBarrier())){
+                check = false;
+                break;
+            }    
+        }
+        if((!layout[3][2].getisBarrier())){
+            check = false;
+        }
+
+        
+
+        //Check Structure2
+        for (int i = 5; i<8; i++){
+            if(!(layout[i][2].getisBarrier() || layout[i][4].getisBarrier())){
+                check = false;
+                break;
+            }
+        }
+
+
+        assertEquals(true, check);
+    }
+
+
+
 
 }
