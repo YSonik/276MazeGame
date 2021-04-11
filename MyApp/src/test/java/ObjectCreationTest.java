79f67d601cf1b13f5e2e80279dee3af3c05f9524
Tian <tza36@sfu.ca>
2021-04-10 18:41:02 -0700

    add setter and label check

--- MyApp/src/test/java/ObjectCreationTest.java
+++ MyApp/src/test/java/ObjectCreationTest.java
@@ -7,7 +7,17 @@ public class ObjectCreationTest {
         Mouse newMouse = new Mouse(3,4);
         assertEquals(newMouse.getCurrentX(),3);
         assertEquals(newMouse.getCurrentY(),4);
+        assertEquals(newMouse.mouseLabel.getX(),300);
+        assertEquals(newMouse.mouseLabel.getY(),500);
         assertEquals(newMouse.getCheeseEaten(),0);
+        newMouse.setCurrentX(4);
+        newMouse.setCurrentY(5);
+        newMouse.setCheeseEaten(2);
+        assertEquals(newMouse.getCurrentX(),4);
+        assertEquals(newMouse.getCurrentY(),5);
+        assertEquals(newMouse.mouseLabel.getX(),400);
+        assertEquals(newMouse.mouseLabel.getY(),600);
+        assertEquals(newMouse.getCheeseEaten(),2);
     }
 
     @Test
@@ -15,6 +25,14 @@ public class ObjectCreationTest {
         Cat newCat = new Cat(3,4,"Images/cat1.png");
         assertEquals(newCat.getCurrentX(),3);
         assertEquals(newCat.getCurrentY(),4);
+        assertEquals(newCat.catLabel.getX(),300);
+        assertEquals(newCat.catLabel.getY(),500);
+        newCat.setCurrentX(4);
+        newCat.setCurrentY(5);
+        assertEquals(newCat.getCurrentX(),4);
+        assertEquals(newCat.getCurrentY(),5);
+        assertEquals(newCat.catLabel.getX(),400);
+        assertEquals(newCat.catLabel.getY(),600);
     }
 
     @Test
@@ -22,7 +40,17 @@ public class ObjectCreationTest {
         Cheese newCheese = new Cheese(3,4,5);
         assertEquals(newCheese.getX(),3);
         assertEquals(newCheese.getY(),4);
+        assertEquals(newCheese.cheeseLabel.getX(),400);
+        assertEquals(newCheese.cheeseLabel.getY(),400);
         assertEquals(newCheese.getValue(),5);
+        newCheese.setX(4);
+        newCheese.setY(5);
+        newCheese.setValue(6);
+        assertEquals(newCheese.getX(),4);
+        assertEquals(newCheese.getY(),5);
+        assertEquals(newCheese.cheeseLabel.getX(),400);
+        assertEquals(newCheese.cheeseLabel.getY(),400);
+        assertEquals(newCheese.getValue(),6);
     }
 
     @Test
@@ -30,7 +58,17 @@ public class ObjectCreationTest {
         OrganicCheese newOrganicCheese = new OrganicCheese(3,4,5);
         assertEquals(newOrganicCheese.getX(),3);
         assertEquals(newOrganicCheese.getY(),4);
+        assertEquals(newOrganicCheese.organicLabel.getX(),400);
+        assertEquals(newOrganicCheese.organicLabel.getY(),400);
         assertEquals(newOrganicCheese.getValue(),5);
+        newOrganicCheese.setX(4);
+        newOrganicCheese.setY(5);
+        newOrganicCheese.setValue(6);
+        assertEquals(newOrganicCheese.getX(),4);
+        assertEquals(newOrganicCheese.getY(),5);
+        assertEquals(newOrganicCheese.organicLabel.getX(),400);
+        assertEquals(newOrganicCheese.organicLabel.getY(),400);
+        assertEquals(newOrganicCheese.getValue(),6);
     }
 
     @Test
@@ -38,6 +76,14 @@ public class ObjectCreationTest {
         MouseTrap newMouseTrap = new MouseTrap(3,4);
         assertEquals(newMouseTrap.getCurrentX(),3);
         assertEquals(newMouseTrap.getCurrentY(),4);
+        assertEquals(newMouseTrap.trapLabel.getX(),400);
+        assertEquals(newMouseTrap.trapLabel.getY(),400);
+        newMouseTrap.setCurrentX(4);
+        newMouseTrap.setCurrentY(5);
+        assertEquals(newMouseTrap.getCurrentX(),4);
+        assertEquals(newMouseTrap.getCurrentY(),5);
+        assertEquals(newMouseTrap.trapLabel.getX(),400);
+        assertEquals(newMouseTrap.trapLabel.getY(),400);
     }
 
     @Test
